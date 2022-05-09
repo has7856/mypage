6faa6e7c865bd0479e9b6314a00fdc7dc3c67fb2
hakyeongjun <has7856@naver.com>
2022-05-09 13:21:39 +0900

    update

---
+++ script/main.js
@@ -0,0 +1,12 @@
+$(function () {
+
+  // fullpage customization
+  $('#fullpage').fullpage({
+    sectionSelector: '.vertical-scrolling',
+    slideSelector: '.horizontal-scrolling',
+    navigation: true,
+    slidesNavigation: true,
+    css3: true,
+    controlArrows: false
+  });
+});
